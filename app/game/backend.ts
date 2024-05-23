1c982adb1a57e0391f7831f7482f9c443bdaa79d
Leg3ndary <ben.zhou.2006@gmail.com>
2024-05-23 15:02:39 -0400

    Remove comment

--- app/game/backend.ts
+++ app/game/backend.ts
@@ -33,7 +33,6 @@ export async function generate(prompt: string) {
     });
     console.log(response);
     return response.data[0];
-    // return "https://oaidalleapiprodscus.blob.core.windows.net/private/org-VQMdSS24ufcelrczDzZAjvR5/user-A03BQAAeqXCbDxXU6DhTXjKw/img-rpW9t27iExXgfUunTanPIc1w.png?st=2024-05-21T13%3A16%3A19Z&se=2024-05-21T15%3A16%3A19Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-05-21T10%3A31%3A29Z&ske=2024-05-22T10%3A31%3A29Z&sks=b&skv=2021-08-06&sig=yAsR09kmmBvO%2BmyPu8UFSoVVvqKm7e1loHemK%2BECEpk%3D";
 }
 
 type openAIResponse = {
